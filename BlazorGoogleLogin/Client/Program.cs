90f00247b81588e927a6e38f8bce574ee075a5c1
Diogo Anderson <diogo.silva@vaivoa.com>
2022-02-04 08:24:47 -0300

    Configurado o cliente para acessar o login do Google

--- BlazorGoogleLogin/Client/Program.cs
+++ BlazorGoogleLogin/Client/Program.cs
@@ -7,5 +7,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddOidcAuthentication(options => {
+	builder.Configuration.Bind("Google", options.ProviderOptions);
+});
 
 await builder.Build().RunAsync();
